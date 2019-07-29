9f5b2a164cb16129c94d3488947d2ba100e417e6
luzagm <luz.aguirre.mo@gmail.com>
2019-07-29 15:50:56 +0200

    add random funtion

--- js/main.js
+++ js/main.js
@@ -2,24 +2,24 @@ const button = document.querySelector(".js-btn");
 const input = document.querySelector(".js-text-box");
 const clue = document.querySelector(".js-clue");
 
-debugger;
-
 function numberGame() {
   const inputValue = parseInt(input.value);
 
   if (inputValue === randomNumber) {
-    return (clue.innerHTML = `¡HAS GANADO, CAMPEONA!`);
+    clue.innerHTML = `¡HAS GANADO, CAMPEONA!`;
   } else if (inputValue > randomNumber) {
-    return (clue.innerHTML = `Demasiado alto`);
+    clue.innerHTML = `Demasiado alto`;
   } else {
-    return (clue.innerHTML = `Demasiado bajo`);
+    clue.innerHTML = `Demasiado bajo`;
   }
 }
 
 function getRandomNumber(max) {
-  return Math.floor(Math.random * max);
+  return Math.floor(Math.random() * max);
 }
 
 const randomNumber = getRandomNumber(100);
 
 button.addEventListener("click", numberGame);
+
+console.log(`Mi número aleatorio es ${randomNumber}`);
